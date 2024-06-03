fd50d6038f707612f47b53ce064280d6944eb2d2
Satrio Budi Pamungkas <satriobeyo@gmail.com>
2024-06-03 23:00:45 +0700

    add morning and night scheduling activity

---
+++ app/src/main/java/com/example/skinsync/activity/users/scheduling/night/NightSchedulingActivity.kt
@@ -0,0 +1,33 @@
+package com.example.skinsync.activity.users.scheduling.night
+
+import android.icu.text.SimpleDateFormat
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.skinsync.R
+import java.util.Locale
+
+class NightSchedulingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_night_scheduling)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textViewDate: TextView = findViewById(R.id.textViewDate)
+
+        // Membuat format tanggal
+        val dateFormat = SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.getDefault())
+        val currentDate = dateFormat.format(java.util.Date())
+
+        // Menampilkan tanggal di TextView
+        textViewDate.text = currentDate
+    }
+}
