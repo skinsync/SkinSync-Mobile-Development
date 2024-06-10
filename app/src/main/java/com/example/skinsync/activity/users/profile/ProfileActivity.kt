ebf21d032f79d563672a0bf34f75395aa72898b2
Satrio Budi Pamungkas <satriobeyo@gmail.com>
2024-06-10 22:13:40 +0700

    intent data profile

--- app/src/main/java/com/example/skinsync/activity/users/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/skinsync/activity/users/profile/ProfileActivity.kt
@@ -1,30 +1,96 @@
 package com.example.skinsync.activity.users.profile
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.skinsync.R
+import com.example.skinsync.activity.MainActivity
 import com.example.skinsync.activity.users.LoginActivity
 import com.example.skinsync.activity.users.profile.edit.EditProfileActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var profileImageView: ImageView
+    private lateinit var usernameTextView: TextView
+    private lateinit var birthDateTextView: TextView
+    private lateinit var genderTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var emailDesc2: TextView
+    private lateinit var passwordTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        profileImageView = findViewById(R.id.avatarImageProfile)
+        usernameTextView = findViewById(R.id.usernameProfile)
+        birthDateTextView = findViewById(R.id.dataBirthDate)
+        genderTextView = findViewById(R.id.genderDesc)
+        emailTextView = findViewById(R.id.gmail_textView)
+        emailDesc2 = findViewById(R.id.emailDesc2)
+        passwordTextView = findViewById(R.id.passwordDesc)
+
+        // Set up button back
+        val backButton = findViewById<ImageView>(R.id.back)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) // Memulai MainActivity
         }
 
-        // Set OnClickListener for the edit button
         findViewById<Button>(R.id.buttonEdit).setOnClickListener {
-            startActivity(Intent(this, EditProfileActivity::class.java))
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+
+        // Terima data yang dikirim dari EditProfileActivity
+        intent.extras?.let {
+            val imageUri = it.getString("uploadedImageUri")
+            val username = it.getString("username")
+            val birthDate = it.getString("selectedDate")
+            val gender = it.getString("gender")
+            val email = it.getString("email")
+            val password= it.getString("password")
+
+            imageUri?.let { uri ->
+                profileImageView.setImageURI(Uri.parse(uri))
+            }
+            usernameTextView.text = username
+            birthDateTextView.text = birthDate
+            genderTextView.text = gender
+            emailTextView.text = email
+            emailDesc2.text = email
+            passwordTextView.text = password
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            data?.extras?.let {
+                val imageUri = it.getString("uploadedImageUri")
+                val username = it.getString("username")
+                val birthDate = it.getString("selectedDate")
+                val gender = it.getString("gender")
+                val email = it.getString("email")
+                val password= it.getString("password")
+
+                imageUri?.let { uri ->
+                    profileImageView.setImageURI(Uri.parse(uri))
+                }
+                usernameTextView.text = username
+                birthDateTextView.text = birthDate
+                genderTextView.text = gender
+                emailTextView.text = email
+                emailDesc2.text = email
+                passwordTextView.text = password
+            }
         }
     }
 }
--- app/src/main/java/com/example/skinsync/activity/users/profile/edit/EditProfileActivity.kt
+++ app/src/main/java/com/example/skinsync/activity/users/profile/edit/EditProfileActivity.kt
@@ -1,12 +1,16 @@
 package com.example.skinsync.activity.users.profile.edit
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.icu.util.Calendar
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.DatePicker
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,16 +18,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.skinsync.R
+import com.example.skinsync.activity.users.profile.ProfileActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class EditProfileActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     private lateinit var pickDateButton: Button
     private lateinit var uploadImageTV: TextView
     private lateinit var uploadImage: ImageView
+    private var uploadedImageUri: Uri? = null
 
     private val selectImageLauncher =
         this.registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri?.let {
+                uploadedImageUri = it
                 uploadImage.setImageURI(it)
             }
         }
@@ -54,6 +62,48 @@ class EditProfileActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListe
         uploadImageTV.setOnClickListener {
             selectImageFromGallery()
         }
+
+        val buttonSave = findViewById<Button>(R.id.buttonSave)
+        val buttonCancel = findViewById<Button>(R.id.buttonCancel)
+        val backButton = findViewById<ImageView>(R.id.back)
+
+        buttonSave.setOnClickListener {
+            // Mengambil nilai dari setiap elemen yang diedit
+            val username = findViewById<TextInputEditText>(R.id.usernameEditText).text.toString()
+            val selectedDate = findViewById<Button>(R.id.pickDate).text.toString()
+            val genderRadioGroup = findViewById<RadioGroup>(R.id.genderRadioGroup)
+            val selectedRadioButtonId = genderRadioGroup.checkedRadioButtonId
+            val selectedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
+            val gender = selectedRadioButton.text.toString()
+            val email = findViewById<TextInputEditText>(R.id.emailEditText).text.toString()
+            val password = findViewById<TextInputEditText>(R.id.passwordEditText).text.toString()
+
+            // Mengirim data menggunakan Intent
+            val intent = Intent().apply {
+                putExtra("uploadedImageUri", uploadedImageUri.toString())
+                putExtra("username", username)
+                putExtra("selectedDate", selectedDate)
+                putExtra("gender", gender)
+                putExtra("email", email)
+                putExtra("password", maskPassword(password))
+            }
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
+        buttonCancel.setOnClickListener {
+            navigateToProfileActivity()
+        }
+
+        backButton.setOnClickListener {
+            navigateToProfileActivity()
+        }
+    }
+
+    private fun navigateToProfileActivity() {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
@@ -63,5 +113,10 @@ class EditProfileActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListe
 
     private fun selectImageFromGallery() {
         selectImageLauncher.launch("image/*")
+    }
+
+    // Fungsi untuk menyamarkan password
+    private fun maskPassword(password: String): String {
+        return "*".repeat(password.length)
     }
 }
