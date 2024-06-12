1913d6b522d9395c1069a9122b056890e94ec00e
Muhammad Razi Rizzardi <114091178+mrazi25@users.noreply.github.com>
2024-06-12 18:19:27 +0700

    fix(article): feature article modification

--- app/src/main/java/com/example/skinsync/activity/admin/article/CreateArticleAdminActivity.kt
+++ app/src/main/java/com/example/skinsync/activity/admin/article/CreateArticleAdminActivity.kt
@@ -1,21 +1,101 @@
 package com.example.skinsync.activity.admin.article
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.skinsync.R
+import com.example.skinsync.data.articleadmin.ArticleAdminRepository
+import com.example.skinsync.databinding.ActivityArticleAdminBinding
+import com.example.skinsync.databinding.ActivityCreateArticleBinding
+import com.example.skinsync.viewmodel.ViewModelFactory
 
 class CreateArticleAdminActivity : AppCompatActivity() {
+    private val PERMISSION_REQUEST_CODE = 1001
+    private val PICK_IMAGE_REQUEST_CODE = 1002
+    private val viewModel by viewModels<ArticleAdminViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityCreateArticleBinding
+    private lateinit var uri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_create_article)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+        binding = ActivityCreateArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        buttonConfig()
+    }
+
+    private fun checkAndRequestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+        } else {
+            openGallery()
         }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                openGallery()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+            selectedImageUri?.let {
+                binding.ivPreviewImageCa.setImageURI(it)
+                uri = it
+                println(it)
+            }
+        }
+    }
+
+    private fun buttonConfig() {
+        with(binding){
+            chooseImage.setOnClickListener {
+                checkAndRequestPermissions()
+            }
+            btnCreateCa.setOnClickListener {
+                val judul = edJudulCa.text.toString()
+                val deskripsi = edDeskripsiCa.text.toString()
+                val url = edUrlCa.text.toString()
+                if (judul == "" || deskripsi == "" || url == "") {
+                    showToast("Input anda belum lengkap")
+                } else {
+                    try {
+                        viewModel.addArticle(judul, deskripsi, deskripsi, url)
+                    } catch (e: Error){
+                        Log.e("Add Artcle: ", e.message!!)
+                    }
+                    showToast("Artikel berhasil ditambahkan!")
+
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/skinsync/data/setup/ApiConfig.kt
+++ app/src/main/java/com/example/skinsync/data/setup/ApiConfig.kt
@@ -9,35 +9,27 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-object ApiConfig {
-    private const val BASE_URL = "https://api-dot-skinsync-424905.et.r.appspot.com/"
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+object ApiConfig {
+    private val BASE_URL = "https://api-dot-skinsync-424905.et.r.appspot.com/"
 
     fun getApiService(): ApiService {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            setLevel(HttpLoggingInterceptor.Level.BODY)
-        }
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
-            .addInterceptor(ErrorInterceptor())
             .build()
-
-        return retrofit.newBuilder().client(client).build().create(ApiService::class.java)
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
     }
 
     fun getApiService(token: String): ApiService {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            setLevel(HttpLoggingInterceptor.Level.BODY)
-        }
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val authInterceptor = Interceptor { chain ->
             val request = chain.request().newBuilder()
                 .addHeader("Authorization", "Bearer $token")
@@ -45,68 +37,14 @@ object ApiConfig {
             chain.proceed(request)
         }
         val client = OkHttpClient.Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
             .addInterceptor(authInterceptor)
-            .addInterceptor(ErrorInterceptor())
             .build()
-
-        return retrofit.newBuilder().client(client).build().create(ApiService::class.java)
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
     }
 }
-
-class ErrorInterceptor : Interceptor {
-    @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val response = chain.proceed(request)
-
-        if (!response.isSuccessful) {
-            val responseBody = response.body?.string()
-            // Log or handle the error response accordingly
-            // For example, you can log the entire response
-            println("Error Response: ${response.code} - $responseBody")
-        }
-        return response
-    }
-}
-//object ApiConfig {
-//    private val BASE_URL = "https://api-dot-skinsync-424905.et.r.appspot.com/"
-//
-//    fun getApiService(): ApiService {
-//        val loggingInterceptor =
-//            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//        val client = OkHttpClient.Builder()
-//            .addInterceptor(loggingInterceptor)
-//            .build()
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .client(client)
-//            .build()
-//        return retrofit.create(ApiService::class.java)
-//    }
-//
-//    fun getApiService(token: String): ApiService {
-//        val loggingInterceptor =
-//            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//        val authInterceptor = Interceptor { chain ->
-//            val request = chain.request().newBuilder()
-//                .addHeader("Authorization", "Bearer $token")
-//                .build()
-//            chain.proceed(request)
-//        }
-//        val client = OkHttpClient.Builder()
-//            .addInterceptor(loggingInterceptor)
-//            .addInterceptor(authInterceptor)
-//            .build()
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .client(client)
-//            .build()
-//        return retrofit.create(ApiService::class.java)
-//    }
-//}
